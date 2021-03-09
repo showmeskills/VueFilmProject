372e759b39c903b0d19e96219934d8feae3e9d4f
Terrance <465446853yan@gmail.com>
2021-03-09 16:02:24 +1100

    vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,4 @@
-//const debug = process.env.NODE_ENV !== 'production'
 module.exports = {
-    // //basic path
-    // publicPath:'./',
-    // //run build files name
-    // outputDir:'dist',
-    // //static files 
-    // assetsDir:'static',
-    // //home page
-    // indexPath:'index.html',
-    // //file name hash
-    // filenameHashing:true,
-    // lintOnSave: false, // turn on eslint check
-    // transpileDependencies: [], // default:babel-loader,ignore:mode_modules
-    // productionSourceMap: true, // construct source map
-    // configureWebpack: config => {
-    //     if (debug) { 
-    //         config.devtool = 'cheap-module-eval-source-map'
-    //     } else {
-    //         config.devtool = 'eval-source-map'
-    //     }
-        
-    // },
     devServer:{
         open: true
     }
