4b8e3d5d4802f6409823aa356ceb54fec76a6bc7
Terrance <465446853yan@gmail.com>
2021-03-19 01:12:32 +1100

    deployed

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-    publicPath: '/', // 根域上下文目录
-    outputDir: 'dist', // 构建输出目录
-    assetsDir: 'assets', // 
+    publicPath: './', // 根域上下文目录 
     devServer:{
         open: true
     }
